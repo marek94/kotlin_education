16c8682da44d21ad10d477a6a44a996ea0d56b11
marek94 <m.pawluch@wasiak-gruppe.de>
2018-09-27 15:22:16 +0200

    synchronize project with gradle files, recycler view initialization

--- app/src/main/java/com/example/mpawluch/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/mpawluch/weatherapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mpawluch.weatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
     }
 }
